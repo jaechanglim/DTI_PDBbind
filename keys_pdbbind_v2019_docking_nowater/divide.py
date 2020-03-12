aac6a369ad73907f414c207787b7542e123089ab
jaechang <ljchang94@kaist.ac.kr>
2020-03-12 17:19:06 +0900

    keys_pdbbind_v2019_docking_nowater is modified

--- keys_pdbbind_v2019_docking_nowater/divide.py
+++ keys_pdbbind_v2019_docking_nowater/divide.py
@@ -4,7 +4,7 @@ import pickle
 train_keys = pickle.load(open('../keys_pdbbind_v2019/train_keys.pkl', 'rb'))
 test_keys = pickle.load(open('../keys_pdbbind_v2019/test_keys.pkl', 'rb'))
 
-keys = glob.glob('/home/wykgroup/udg/mseok/data/data_pdbbind_v2016_docking_nowater/docking_data/*')
+keys = glob.glob('/home/wykgroup/udg/mseok/data/data_pdbbind_v2016_docking_nowater/data/*')
 keys = [k.split('/')[-1] for k in keys]
 train_keys = [k for k in keys if k.split('_')[0] in train_keys]
 test_keys = [k for k in keys if k.split('_')[0] in test_keys]
