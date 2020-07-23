28ad5df6268fc95bea68895e74b3bd5783bb4035
SeonghwanSeo <shwan0106@kaist.ac.kr>
2020-07-23 12:03:49 +0900

    kets for random_pubchem_nowater is added

---
+++ keys_pdbbind_random_pubchem_nowater/divide.py
@@ -0,0 +1,31 @@
+import pickle
+from collections import Counter
+import random
+from rdkit.Chem.Scaffolds.MurckoScaffold import MurckoScaffoldSmilesFromSmiles
+from rdkit import Chem
+import numpy as np
+import glob
+import gc
+import time
+import sys
+import os
+random.seed(0)
+#train and test pdbs
+pdbbind_train_keys = pickle.load(open(f'../keys_pdbbind_v2019/train_keys.pkl', 'rb'))
+pdbbind_test_keys = pickle.load(open(f'../keys_pdbbind_v2019/test_keys.pkl', 'rb'))
+
+#choose train keys
+keys = glob.glob('../../data_pdbbind_random_pubchem_nowater/data/*')[:]
+keys = [k.split('/')[-1] for k in keys]
+train_keys = [k for k in keys if k.split('_')[0] in pdbbind_train_keys] 
+train_mol_ids = set([k.split('_')[1] for k in train_keys])
+
+#choose test keys
+test_keys = [k for k in keys if k.split('_')[0] in pdbbind_test_keys]
+test_keys = [k for k in test_keys if k.split('_')[1] not in train_mol_ids]
+
+print (len(train_keys), len(test_keys))
+with open(f'train_keys.pkl', 'wb') as fp:
+    pickle.dump(train_keys, fp)
+with open(f'test_keys.pkl', 'wb') as fp:
+    pickle.dump(test_keys, fp)
